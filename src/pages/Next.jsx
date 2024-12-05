b935bb1e37010c31db23f7e55dd0959d5b443dfc
Gagan Suman <fbdgagansuman@gmail.com>
2024-12-05 15:12:46 +0530

    next

--- src/pages/Next.jsx
+++ src/pages/Next.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Hero from "../Components/Next/Hero"; // Assuming Hero component is defined in a separate file
+import Hero from "../components/Next/Hero"; // Assuming Hero component is defined in a separate file
 import Navbar from "../components/Next/Navbar";
 import Section2 from "../components/Next/Section2";
 
-import './Next.css'
+
 
 function Next() {
   return (
