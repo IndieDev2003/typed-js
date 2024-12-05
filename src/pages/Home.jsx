2a90da8cb247144138e58a7758efbd598fd6badb
Gagan Suman <fbdgagansuman@gmail.com>
2024-12-05 15:17:20 +0530

    text-center

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="h-screen w-screen flex items-center justify-center flex-col outfit backdrop-blur-lg">
-      <h1 className="text-5xl outfit">This is Home Page</h1>
+      <h1 className="text-5xl outfit text-center w-full">This is Home Page</h1>
       <div className="flex mt-4 gap-4">
         <Link to="/letter">
           <h2 className="border border-gray-400 cursor-pointer px-8 py-2 rounded-lg hover:bg-gray-400 hover:text-white">
